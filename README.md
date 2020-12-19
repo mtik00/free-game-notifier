ac25746851db0f71c631934cfea05e0b8397644f
Tim McFadden <mtik00@users.noreply.github.com>
2020-12-19 11:53:26 -0700

    moving to envargparse

--- README.md
+++ README.md
@@ -2,3 +2,29 @@
 
 This module us used to scrape a certain Steam Community RSS feed and post
 the results to a Slack channel.
+
+## Configuration
+
+You can configure the application using only environment variables, only the
+command line, or a combination of both where command-line args take precedence
+over environment variables.
+
+The webhook parameter is not required.  Debug output will be logged instead of
+sending the message.  You probably want to also add `--debug`.
+
+### Command-line Arguments
+The following arguments are used to configure the run:
+
+*   `--debug` : Enables debug output.
+*   `---url <path or URL>` : The RSS feed to scrape.  Can be a local file path  
+    or remote URL.
+*   `--webhook <URL>` : The URL of the Slack webhook to which to send the message.
+
+### Environment Variables
+
+*   `SFN_APP_DEBUG`: Whether or not to enable debug output.  This defaults to  
+    `False`.  You can enable it by setting it to anything _other_ than "no", "0",
+    or "false" (case-insensitive, only the first character is tested).
+*   `SFN_APP_URL`: The URL to scrape.  This can be a local file path 
+    or a URL.
+*   `SFN_APP_WEBHOOK`: The Slack webhook to send the information to.
--- steam_free_notifier/app.py
+++ steam_free_notifier/app.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import argparse
 import os
 import re
 import sys
@@ -8,6 +7,7 @@ import logging
 
 import feedparser
 import requests
+from envargparse import EnvArgParser
 
 from .logger import get_logger
 
@@ -18,27 +18,59 @@ SLACK_WEBHOOK = os.environ.get("SLACK_WEBHOOK") or None
 LOGGER = get_logger()
 
 
+def bool_envvar_to_envargparser(*args):
+    """
+    EnvArgParser doesn't do a good job at handling flags.  This function will
+    convert "sane" inputs to something that EnvArgParser will understand as a
+    boolean.
+
+    EnvArgParser will treat an empty string as `True`, and everthing else as an
+    error.
+
+    The end result of this function is either removing the environment variable
+    or setting it to an empty string.
+    """
+    for name in args:
+        if (value := os.environ.get(name)) is not None:
+            if (not value) or (value.lower()[0] in ["n", "0", "f"]):
+                # remove the env var if it's anything that's "False"
+                os.environ.pop(name)
+            else:
+                # Assume that the user wishes to enable debug.
+                # The env var must be an empty string for EnvArgParser to successfully
+                # convert it.
+                os.environ[name] = ""
+
+
 def parse_args(args=sys.argv[1:]):
-    parser = argparse.ArgumentParser(description="Process some integers.")
+    bool_envvar_to_envargparser("SFN_APP_DEBUG")
+
+    parser = EnvArgParser(description="Scrap an RSS feed and post to a webhook.")
     parser.add_argument(
-        "-d", "--debug", help="Print debugging information.", action="store_true"
-    )
-    parser.add_argument(
-        "--no-slack",
-        help="Don't actually send the Slack message, just log it.",
+        "-d",
+        "--debug",
+        help="Print debugging information.  Also set by $SFN_APP_DEBUG",
+        env_var="SFN_APP_DEBUG",
+        default=False,
         action="store_true",
     )
     parser.add_argument(
-        "--filename", help="The name of the local file to read instead of hitting Steam"
+        "--url",
+        help="The file or URL from which to read the RSS feed.  Also set by $SFN_APP_URL",
+        env_var="SFN_APP_URL",
+        required=True,
     )
     parser.add_argument(
         "--webhook",
-        help="The Slack webhook to post the message to.  Also: ${SLACK_WEBHOOK}",
+        help="The Slack webhook to post the message to.  Also set by $SFN_APP_WEBHOOK",
+        env_var="SFN_APP_WEBHOOK",
     )
+
     return parser.parse_args(args)
 
 
-def main(slack=True, feed_url=URL, webook=SLACK_WEBHOOK):
+def main(feed_url, webook=None):
+    LOGGER.debug(f"reading feed from {feed_url}")
     feed = feedparser.parse(feed_url)
     first_item = feed["items"][0]
 
@@ -70,32 +102,25 @@ def main(slack=True, feed_url=URL, webook=SLACK_WEBHOOK):
         ]
     }
 
-    if slack:
+    if webook:
         LOGGER.debug("Sending slack message...")
         response = requests.post(webook, json=data)
         response.raise_for_status()
         LOGGER.debug("...done")
     else:
-        LOGGER.debug("Not sending slack message")
+        LOGGER.debug("Not posting to webhook")
         LOGGER.debug(data)
 
 
 def run():
     """Parse the arguments and call the main function."""
     args = parse_args()
+    args.url
 
-    slack = not args.no_slack
-
-    feed_url = URL
-    if args.filename:
-        feed_url = args.filename
-
-    webook = args.webhook or SLACK_WEBHOOK
-    
     if args.debug:
         LOGGER.setLevel(logging.DEBUG)
 
-    main(slack=slack, feed_url=feed_url, webook=webook)
+    main(feed_url=args.url, webook=args.webhook)
 
 
 if __name__ == "__main__":
