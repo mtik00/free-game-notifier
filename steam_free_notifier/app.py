049f8bd765b72830cb4f867dff79bdd37381fa06
Tim McFadden <mtik00@users.noreply.github.com>
2020-12-22 09:20:50 -0700

    switching from EnvArgParse to typer

--- steam_free_notifier/app.py
+++ steam_free_notifier/app.py
@@ -4,12 +4,12 @@ import logging
 import os
 import re
 import sys
-from pprint import pformat
 import time
+from pprint import pformat
 
 import feedparser
 import requests
-from envargparse import EnvArgParser
+import typer
 
 from .cache import Cache
 from .item import Item
@@ -18,74 +18,18 @@ from .logger import get_logger
 LOGGER = get_logger()
 
 
-def bool_envvar_to_envargparser(*args):
-    """
-    EnvArgParser doesn't do a good job at handling flags.  This function will
-    convert "sane" inputs to something that EnvArgParser will understand as a
-    boolean.
-
-    EnvArgParser will treat an empty string as `True`, and everthing else as an
-    error.
-
-    The end result of this function is either removing the environment variable
-    or setting it to an empty string.
-    """
-    for name in args:
-        if (value := os.environ.get(name)) is not None:
-            if (not value) or (value.lower()[0] in ["n", "0", "f"]):
-                # remove the env var if it's anything that's "False"
-                os.environ.pop(name)
-            else:
-                # Assume that the user wishes to enable debug.
-                # The env var must be an empty string for EnvArgParser to successfully
-                # convert it.
-                os.environ[name] = ""
-
-
-def parse_args(args=sys.argv[1:]):
-    bool_envvar_to_envargparser("SFN_APP_DEBUG", "SFN_APP_VERBOSE")
-
-    parser = EnvArgParser(description="Scrap an RSS feed and post to a webhook.")
-    parser.add_argument(
-        "-d",
-        "--debug",
-        help="Print debugging information.  Also set by $SFN_APP_DEBUG",
-        env_var="SFN_APP_DEBUG",
-        default=False,
-        action="store_true",
-    )
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        help="Print more debugging information.  Also set by $SFN_APP_VERBOSE",
-        env_var="SFN_APP_VERBOSE",
-        default=False,
-        action="store_true",
-    )
-    parser.add_argument(
-        "--url",
-        help="The file or URL from which to read the RSS feed.  Also set by $SFN_APP_URL",
-        env_var="SFN_APP_URL",
-        required=True,
-    )
-    parser.add_argument(
-        "--webhook",
-        help="The Slack webhook to post the message to.  Also set by $SFN_APP_WEBHOOK",
-        env_var="SFN_APP_WEBHOOK",
-    )
-    parser.add_argument(
-        "--cache-path",
-        help="The path to the cache file.  Also set by $SFN_APP_CACHE_PATH",
-        env_var="SFN_APP_CACHE_PATH",
-    )
-    return parser.parse_args(args)
-
-
-def main(feed_url, webhook=None, verbose=False, cache_path=None):
+def main(
+    url: str = typer.Option(..., envvar="SFN_APP_URL"),
+    webhook: str = typer.Option(None, envvar="SFN_APP_WEBHOOK"),
+    verbose: bool = typer.Option(False, envvar="SFN_APP_VERBOSE"),
+    cache_path: str = typer.Option(None, envvar="SFN_APP_CACHE_PATH"),
+):
     cache = Cache(path=cache_path)
+    if verbose:
+        LOGGER.setLevel(logging.DEBUG)
 
-    LOGGER.debug(f"reading feed from {feed_url}")
-    feed = feedparser.parse(feed_url)
+    LOGGER.debug(f"reading feed from {url}")
+    feed = feedparser.parse(url)
 
     # We're only interested in the first item from the RSS feed.
     item = Item.from_rss_element(feed["items"][0])
@@ -119,18 +63,7 @@ def main(feed_url, webhook=None, verbose=False, cache_path=None):
 
 
 def run():
-    """Parse the arguments and call the main function."""
-    args = parse_args()
-
-    if args.debug or args.verbose:
-        LOGGER.setLevel(logging.DEBUG)
-
-    main(
-        feed_url=args.url,
-        webhook=args.webhook,
-        verbose=args.verbose,
-        cache_path=args.cache_path,
-    )
+    typer.run(main)
 
 
 if __name__ == "__main__":
