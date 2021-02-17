a943b71a26c883bcfc068ea624947fad74f7ae16
Tim McFadden <mtik00@users.noreply.github.com>
2021-02-16 21:25:04 -0700

    Adding Ubisoft icon
    ...also refactoring default ICONs to mimic the settings.

--- tests/icons/test_icons.py
+++ tests/icons/test_icons.py
@@ -3,7 +3,7 @@ from free_game_notifier.icons import icon_from_url
 
 def test_steam_favicon(mock_request, configuration):
     """Make sure the default is working"""
-    x = icon_from_url('https://store.steampowered.com/some/link/page.html')
+    x = icon_from_url("https://store.steampowered.com/some/link/page.html")
     assert x == "https://store.steampowered.com/favicon.ico"
 
 
@@ -16,3 +16,8 @@ def test_steam_settings(mock_request, configuration):
 def test_invalid_url(mock_request_raise, configuration):
     x = icon_from_url("https://xyz-asdf-123.edu/store/games/bingo.html")
     assert x is None
+
+
+def test_default_ubisoft(mock_request, configuration):
+    x = icon_from_url("https://store.ubi.com/us/game/some_game.html")
+    assert x == "https://www.ubisoft.com/favicon.ico"
